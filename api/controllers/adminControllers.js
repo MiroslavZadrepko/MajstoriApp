40ff7fbb8d0d8cf6a6a3a59d04b028aad5167c25
Miroslav Zadrepko <miroslav.zadrepko@gmail.com>
2023-02-24 13:22:40 +0000

    find and delete and replace craftsman

--- api/controllers/adminControllers.js
+++ api/controllers/adminControllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const TmpCraftsman = require('../models/tmpCraftsmanModel') //source
+const Craftsman = require('../models/craftsmanModel')
 
 /**GET TMP CRAFTSMEN***********
  * GET /api/admin/tmpcraftsmen*
@@ -35,12 +36,16 @@ const deleteTmpCrafstman = asyncHandler(async (req, res) => {
  ***********************************/
 const addCraftsman = asyncHandler(async (req, res) => {
 
-    const craftsman = await TmpCraftsman.findById(req.params.id);
+    const craftsman = await TmpCraftsman.findByIdAndDelete(req.params.id);
 
     if (!craftsman) {
         res.status(400)
         throw new Error('craftsman not found')
     };
+
+    // const destInstance = new Craftsman(craftsman);
+    // await destInstance.save();
+    // if up goes, next part is out
 
     const filter = { _id: craftsman._id };
     await TmpCraftsman.aggregate([
@@ -53,6 +58,29 @@ const addCraftsman = asyncHandler(async (req, res) => {
             }
         },
     ]);
+
+    /**
+     // Assume the following document exists in the source collection
+{
+  "_id": ObjectId("1234567890"),
+  "name": "John Doe",
+  "age": 30,
+  "email": "johndoe@example.com"
+}
+
+// Cut and paste the document from the source to the destination model
+const SourceModel = mongoose.model("SourceModel", sourceSchema);
+const DestinationModel = mongoose.model("DestinationModel", destinationSchema);
+
+try {
+  const document = await SourceModel.findOneAndDelete({ "_id": ObjectId("1234567890") });
+  const destinationInstance = new DestinationModel(document);
+  await destinationInstance.save();
+} catch (err) {
+  console.error(err);
+}
+
+     */
 
     res.status(200).json({ id: req.params.id });
 
